6d22633daedd1bdb49ec94d5248c3b5f4ce1a450
bajtolini <bajtolini@gmail.com>
2013-07-19 12:25:37 +0200

    First attempt in creating server+chat, need to make few changes.

---
+++ Chat/src/client/ClientApplet.java
@@ -0,0 +1,14 @@
+package client;
+
+import java.applet.*;
+import java.awt.*;
+
+public class ClientApplet extends Applet {
+
+    public void init() {
+        String host = getParameter("host");
+        int port = Integer.parseInt(getParameter("port"));
+        setLayout(new BorderLayout());
+        add("Center", new Client(host, port));
+    }
+}
